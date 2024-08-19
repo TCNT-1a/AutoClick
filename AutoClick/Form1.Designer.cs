1ff7217a77078c5a217b4ea2479be2dcdffc2471
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-08-19 23:42:02 +0700

    up

---
+++ AutoClick/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace AutoClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(22, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Auto Click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tọa độ: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(22, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nhấn G để lấy tọa độ cần click";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(22, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(341, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Khi đang click, bấm ESC để dừng click";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown1.Location = new System.Drawing.Point(106, 62);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 34);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(232, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Số lần click";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private NumericUpDown numericUpDown1;
+        private Label label4;
+    }
+}
---
+++ AutoClick/Form1.cs
@@ -0,0 +1,114 @@
+﻿namespace AutoClick
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Windows.Forms;
+    using Timer = System.Windows.Forms.Timer;
+
+    //using Timer = System.Threading.Timer;
+
+    public partial class Form1 : Form
+    {
+        
+        private int X;
+        private int Y;
+        private int N;
+        [StructLayout(LayoutKind.Sequential)]
+        private struct INPUT
+        {
+            public uint type;
+            public MOUSEINPUT mi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        private const uint INPUT_MOUSE = 0;
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+        private const int VK_ESCAPE = 0x1B; // Mã phím ESC
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Đăng ký sự kiện KeyDown cho form
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            // Đảm bảo rằng form nhận sự kiện KeyDown
+            this.KeyPreview = true;
+
+        }
+        // Phương thức thực hiện click tại tọa độ (x, y) n lần
+        public void ClickNTimesWithoutMovingCursor(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                // Kiểm tra nếu phím ESC được nhấn
+                if (GetAsyncKeyState(VK_ESCAPE) != 0)
+                {
+                    Console.WriteLine("Vòng lặp bị dừng bởi phím ESC.");
+                    break; // Thoát khỏi vòng lặp nếu phím ESC được nhấn
+                }
+
+                // Tạo sự kiện click chuột mà không di chuyển chuột
+                INPUT[] inputs = new INPUT[2];
+
+                inputs[0].type = INPUT_MOUSE;
+                inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+                inputs[1].type = INPUT_MOUSE;
+                inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+                SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+                // Tạm dừng giữa các lần click
+                System.Threading.Thread.Sleep(100); // Dừng 100ms giữa mỗi lần click (có thể điều chỉnh)
+            }
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.G)
+            {
+                Point mousePosition = Cursor.Position;
+                this.X = mousePosition.X;
+                this.Y = mousePosition.Y;
+                label1.Text = $"({X},{Y})";
+                button1.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(X == 0 && Y == 0)
+                button1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClickNTimesWithoutMovingCursor(100);
+            N = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            N = (int)numericUpDown1.Value;
+        }
+    }
+}
